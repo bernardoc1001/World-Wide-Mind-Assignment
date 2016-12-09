835fa7ebcda9a654f1d142b8979a401d6e92d94b
bernardoc1001 <bernard.oconnor47@mail.dcu.ie>
2016-12-09 20:00:37 +0000

    AI returning correct action, not executing the move though

---
+++ src/Human-VS-Human.js
@@ -0,0 +1,24 @@
+function Mind() {
+
+
+//--- public functions / interface / API ----------------------------------------------------------
+
+
+    this.newRun = function()
+    {
+    };
+
+    this.endRun = function()
+    {
+    };
+
+
+
+    this.getAction = function ( x )
+    {
+    };
+
+}
+
+
+
